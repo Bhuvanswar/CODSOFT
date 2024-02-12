64ffe8bd1f0fae723aa257a8194305c31e159e5a
ADI Bhuvaneswar <145249303+Bhuvanswar@users.noreply.github.com>
2024-02-12 15:56:08 +0530

    Update Guessing.java

--- Guessing.java
+++ Guessing.java
@@ -1,27 +1,44 @@
 import java.util.*;
+public class Guessing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+         int score = 0;
+        boolean playAgain = true;
 
-public class Guessing{
-    public static void main (String[]args){
-        Scanner sc =new Scanner(System.in);
-        int t=3,x,n,i;
-        x=1+(int)(100*Math.random());
-        System.out.println("let the game begin");
-        System.out.println("Guess the number");
-        for(i=0;i<t;i++){
-            n=sc.nextInt();
-            if(n==x){
-                System.out.println("you win");
-                break;
+        while (playAgain) {
+            int N = random.nextInt(100) + 1;
+            int x = 0;
+            int m = 5;
+            System.out.println("let the game begin");
+            while (x< m) {
+                System.out.print("Enter your guess: ");
+                int n= sc.nextInt();
+                sc.nextLine();
+
+                x++;
+
+                if (n == N) {
+                    System.out.println("You win.");
+                    score++;
+                    break;
+                } else if (n < N) {
+                    System.out.println("Guess is low! Try again.");
+                } else {
+                    System.out.println("Guess is  high! Try again.");
+                }
             }
-            else if (x<n){
-                System.out.println("Guess is high try again");
+
+            if (x>= m) {
+                System.out.println("you are out .\n The correct number was " + N + ".");
             }
-            else if(x>n){
-                System.out.println("Guess is low try again");
-            }
-           
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainInput = sc.nextLine();
+            playAgain = playAgainInput.equalsIgnoreCase("yes");
         }
-        System.out.println("You are out");
-        System.out.println("The number is"+x);
-    }
+
+        System.out.println("Game over! Your score: " + score);
+
+        }
 }
